ccf7c9b2a151370beb8930d89dd42366e72f0463
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2018-03-28 12:16:01 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const { get } = require('axios');
-const URL = 'https://kodaktor.ru/j/users';
+const moment = require('moment');
+const fs = require('fs');
+const USERS_URL = 'https://kodaktor.ru/j/users';
 
-const PORT = 4321;
+const PORT = 7654;
 const app = express();
+
+const myLogger =  (req, res, next) => {
+  console.log('LOGGED : ' + moment().format('DD.MM.YYYY HH:mm:ss') + "\t" + req.url);
+  fs.appendFileSync("log.txt", moment().format('DD.MM.YYYY HH:mm:ss') + " " + req.url + "\n",  "ascii");
+  next();
+};
+
 app
-   .get(/hello/, r => r.res.end('Hello, world!'))
-   .get(/users/, async r => { 
-      const { data: { users: items } } = await get(URL);
-      r.res.render('list', {title: 'Login list', items});
-   })
-   .use(r => r.res.status(404).end('Still not here, sorry!'))
-   .use((e, r, res, n) => res.status(500).end('Error: ${e}'))
-   .set('view engine', 'pug')
-   .listen(process.env.PORT || PORT, () => console.log("My pid is very big : " + process.pid));
+	.use(myLogger)
+   	.get(/users/, async r => {
+    	const { data: { users: items } } = await get(USERS_URL);
+    	r.res.render('list', {title: 'Login list', items});
+   	})
+   	.use(r => r.res.status(404).end('Still not here, sorry!'))
+   	.use((e, r, res, n) => res.status(500).end(`${e}`))
+   	.set('view engine', 'pug')
+   	.listen(process.env.PORT || PORT, () => console.log("Hello, i am here : " + process.pid));
