48fd002da7876c2414cfde96e253645b1c5f4044
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2018-04-15 13:17:05 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,24 +2,37 @@ const express = require('express');
 const { get } = require('axios');
 const moment = require('moment');
 const fs = require('fs');
-const USERS_URL = 'https://kodaktor.ru/j/users';
+const exphbs  = require('express-handlebars');
+
 
 const PORT = 7654;
-const app = express();
-
-const myLogger =  (req, res, next) => {
-  console.log('LOGGED : ' + moment().format('DD.MM.YYYY HH:mm:ss') + "\t" + req.url);
-  fs.appendFileSync("log.txt", moment().format('DD.MM.YYYY HH:mm:ss') + " " + req.url + "\n",  "ascii");
+const URL = 'https://kodaktor.ru/j/users';
+const app = express(); //Godlike
+const Olologger =  (req, res, next) => {
+  fs.appendFile('log.txt', moment().format('DD.MM.YYYY HH:mm:ss') + ': ' + req.url + '\n', (err) => {
+    if (err) throw err;
+    console.log('update log!');
+  });
   next();
 };
 
+app.engine('handlebars', exphbs({layoutsDir: 'listhb'}));
+app.set('view engine', 'handlebars');
+
+
 app
-	.use(myLogger)
-   	.get(/users/, async r => {
-    	const { data: { users: items } } = await get(USERS_URL);
-    	r.res.render('list', {title: 'Login list', items});
-   	})
-   	.use(r => r.res.status(404).end('Still not here, sorry!'))
-   	.use((e, r, res, n) => res.status(500).end(`${e}`))
-   	.set('view engine', 'pug')
-   	.listen(process.env.PORT || PORT, () => console.log("Hello, i am here : " + process.pid));
+	.use(Olologger)
+	.get('/',  (req, res) => {
+  		res.send('Hello! You are on my localhost!');
+	})
+	.get('/log/', r => r.res.end(fs.readFileSync('log.txt')))
+	.get('/hello', r => r.res.end('Hello world'))
+	.get('/hello/:name', r => r.res.end(`Hello, ${r.params.name}`))
+	.get('/listhb', async r => {
+      const {data: {users: items}} = await get(URL);
+      r.res.render('listhb', {users: items})
+     })
+    .use(r => r.res.status(404).end('Still not here, sorry!'))
+    .use((e, r, res, n) => res.status(500).end(`Error: ${e}`))
+
+	.listen(process.env.PORT || PORT, () => console.log('Im working!:)'));
